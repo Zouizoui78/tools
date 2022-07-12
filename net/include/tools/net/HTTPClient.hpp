ecab5c7df3e5888e532e08e88ab1703f3ee27343
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2022-07-12 09:13:14 +0200

    Added HTTPClient.

---
+++ net/include/tools/net/HTTPClient.hpp
@@ -0,0 +1,248 @@
+#ifndef HTTPCLIENT_HPP
+#define HTTPCLIENT_HPP
+
+#include <curl/curl.h>
+#include <functional>
+#include <map>
+#include <string>
+#include <thread>
+
+namespace tools::net {
+
+class HTTPClient;
+
+/**
+ * @brief HTTP reply content packaging class.
+ * 
+ */
+class HTTPReply {
+    public:
+    /**
+     * @brief Default constructor. 
+     */
+    HTTPReply();
+    
+    /**
+     * @brief Construct an HTTPReply object. 
+     */
+    HTTPReply(CURL *handle);
+
+    /**
+     * @brief Destructor.
+     */
+    ~HTTPReply();
+
+    /**
+     * @brief Restore the object attributes to their default values :
+     * res_code = 0 ;
+     * data = "" ;
+     * host = "";
+     * _handle = nullptr
+     */
+    void reset();
+
+    long res_code;
+    std::string data;
+    std::string host;
+
+    private:
+    CURL *_handle;
+
+    // Grant access to _handle to HTTPClient without providing getters.
+    friend class HTTPClient;
+};
+
+using Callback = std::function<void (HTTPReply &)>;
+
+/**
+ * @brief Very simple cURL wrapper for performing HTTP GET and POST requests.
+ * 
+ */
+class HTTPClient {
+    public:
+    
+    /**
+     * @brief Default constructor. 
+     */
+    HTTPClient();
+
+    /**
+     * @brief Destructor.
+     */
+    ~HTTPClient();
+
+    /**
+     * @brief Tell whether the object has successfully initialized or not.
+     * @return true if curl initialization was successful ; false otherwise
+     */
+    bool is_ok();
+
+    /**
+     * @brief Perform a blocking HTTP POST request.
+     * 
+     * @param reply OUTPUT Reply object. Its fields are cleared before the request if performed.
+     * @param url URL to post to.
+     * @param headers headers to send with the request.
+     * @return bool Ok or not.
+     */
+    bool get(HTTPReply &reply, const std::string &url, const std::vector<std::string> &headers = {});
+
+    /**
+     * @brief Perform a blocking HTTP POST request.
+     * 
+     * @param reply OUTPUT Reply object.
+     * @param url URL to post to.
+     * @param post_data Data to send with POST requests.
+     * @param headers Headers to add to the request.
+     * @return bool Ok or not.
+     */
+    bool post(
+        HTTPReply &reply,
+        const std::string &url,
+        const std::string &post_data,
+        const std::vector<std::string> &headers = {}
+    );
+
+    /**
+     * @brief Perform a non-blocking HTTP GET request.
+     * 
+     * @param url URL to post to.
+     * @param callback Called after the request is complete.
+     * @param headers Headers to send with the request.
+     * @return bool Ok or not.
+     */
+    bool get_async(
+        const std::string &url,
+        Callback callback,
+        const std::vector<std::string> &headers = {}
+    );
+
+    /**
+     * @brief Perform a non-blocking HTTP POST request.
+     * 
+     * @param url URL to post to.
+     * @param callback Called after the request is complete.
+     * @param headers Headers to send with the request.
+     * @return bool Ok or not.
+     */
+    bool post_async(
+        const std::string &url,
+        const std::string &post_data,
+        Callback callback,
+        const std::vector<std::string> &headers = {}
+    );
+
+    /**
+     * @brief Return when all pending async requests are done.
+     */
+    void wait_async_requests();
+
+    private:
+
+    /**
+     * @brief Callback called by curl when requests are done.
+     * 
+     * @param data Data received. Does not necesserally contains the entire response,
+     * i.e. this callback can be called several times by curl.
+     * @param size Always 1 according to curl docs.
+     * @param nmemb Size of data.
+     * @param instance Custom object passed by the user. We pass the reply object through it.
+     * @return size_t Size of received data.
+     */
+    static size_t curl_callback(char *data, size_t size, size_t nmemb, void *instance);
+
+    /**
+     * @brief Initialize an easy handle.
+     * 
+     * @return CURL* Created handle ; nullptr if failed.
+     */
+    CURL *easy_handle_init();
+
+    /**
+     * @brief Set the handle's headers.
+     * 
+     * @param handle Handle to modify.
+     * @param headers Strings containing the headers to add to the list.
+     * @return curl_slist * Headers list. The caller should free it when the request is done. nullptr if empty headers vector.
+     */
+    curl_slist *set_easy_handle_headers(CURL *handle, const std::vector<std::string> &headers);
+
+    /**
+     * @brief Set the common easy handle options that
+     * are needed by both sync and async requests.
+     * 
+     * @param handle curl handle to modify.
+     * @param url Request URL.
+     * @param post_data POST request data. If empty the handle is set to send a GET request.
+     * @param reply Passed to curl callback.
+     * @return Ok or not.
+     */
+    bool set_easy_handle_opt(
+        CURL *handle,
+        const std::string &url,
+        const std::string &post_data,
+        const HTTPReply &reply
+    );
+
+    /**
+     * @brief Join and remove terminated threads stored in _threads_map.
+     */
+    void cleanup_threads();
+
+    /**
+     * @brief Parse the url to extract the host.
+     * E.g. if url == "https://example.com:12345/test", it returns "example.com".
+     * 
+     * @param url URL to parse.
+     * @return std::string Host.
+     */
+    std::string get_host_from_url(const std::string &url);
+
+    /**
+     * @brief Internal method used to send synchronous requests with _sync_handle.
+     * 
+     * @param reply Reply object.
+     * @param url URL to send request to.
+     * @param headers Headers to apply on the request.
+     * @param post_data Data to send with POST requests. If empty a GET request is performed.
+     * @return Ok or not.
+     */
+    bool sync_request(
+        HTTPReply &reply,
+        const std::string &url,
+        const std::vector<std::string> &headers = {},
+        const std::string &post_data = ""
+    );
+
+    /**
+     * @brief Internal method used to send asynchronous requests.
+     * 
+     * @param url URL to send request to.
+     * @param callback Called when responses is received.
+     * @param headers Headers to apply on the request.
+     * @param post_data Data to send with POST requests. If empty a GET request is performed.
+     * @return Ok or not.
+     */
+    bool async_request (
+        const std::string &url,
+        Callback callback,
+        const std::vector<std::string> &headers = {},
+        const std::string &post_data = ""
+    );
+
+    // These three attributes are used to handle curl initialisation and cleanup.
+    static std::atomic<uint32_t> _instance_count;
+    static std::atomic<bool> _global_curl_initialized;
+    std::mutex _instances_mutex;
+
+    // Attributes used during async requests to keep track of running threads
+    // and cleaned the terminated ones.
+    std::map<std::thread::id, std::thread> _threads_map;
+    std::vector<std::thread::id> _threads_to_join;
+    std::mutex _threads_map_mutex;
+    std::mutex _threads_to_join_mutex;
+};
+
+} // namespace tools::net
+
+#endif // HTTPCLIENT_HPP
---
+++ net/test/TestHTTPClient.cpp
@@ -0,0 +1,148 @@
+#include <gtest/gtest.h>
+#include "tools/net/HTTPClient.hpp"
+#include "tools/utils/Log.hpp"
+
+namespace test {
+
+using namespace tools::net;
+static auto logger = tools::utils::new_logger("TestHTTPClient");
+
+class TestHTTPClient:   public ::testing::Test
+{
+    protected:
+        TestHTTPClient() {}
+
+        virtual ~TestHTTPClient() {}
+
+        virtual void SetUp() {}
+
+        virtual void TearDown() {}
+
+    public:
+};
+
+std::string expected_google_301 = "<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\n<TITLE>301 Moved</TITLE></HEAD><BODY>\n<H1>301 Moved</H1>\nThe document has moved\n<A HREF=\"https://www.google.com/\">here</A>.\r\n</BODY></HTML>\r\n";
+
+TEST_F(TestHTTPClient, synchronous) {
+    HTTPClient client;
+    ASSERT_TRUE(client.is_ok());
+    HTTPReply reply;
+
+    // We test POST request first to make sure it doesn't break following GET requests
+    // since POST requests implicitly change some curl settings.
+    EXPECT_TRUE(client.post(reply, "https://google.com", "test", { "Accept: application/json" }));
+    EXPECT_EQ(reply.host, "google.com");
+    EXPECT_EQ(reply.res_code, 405);
+
+    EXPECT_TRUE(client.get(reply, "https://google.com", { "Accept: application/json" }));
+    EXPECT_EQ(reply.host, "google.com");
+    EXPECT_EQ(reply.res_code, 301);
+    EXPECT_EQ(reply.data, expected_google_301);
+}
+
+TEST_F(TestHTTPClient, async) {
+    HTTPClient client;
+    ASSERT_TRUE(client.is_ok());
+
+    uint8_t callbacks_called = 0;
+    std::mutex mutex;
+
+    EXPECT_TRUE(client.post_async("https://google.com", "test", [&](HTTPReply &reply) {
+        EXPECT_EQ(reply.host, "google.com");
+        EXPECT_EQ(reply.res_code, 405);
+        std::lock_guard lock(mutex);
+        ++callbacks_called;
+    }, { "Accept: application/json" }));
+
+    EXPECT_TRUE(client.get_async("https://google.com", [&](HTTPReply &reply) {
+        EXPECT_EQ(reply.host, "google.com");
+        EXPECT_EQ(reply.data, expected_google_301);
+        EXPECT_EQ(reply.res_code, 301);
+        std::lock_guard lock(mutex);
+        ++callbacks_called;
+    }, { "Accept: application/json" }));
+
+    client.wait_async_requests();
+
+    EXPECT_EQ(callbacks_called, 2);
+}
+
+TEST_F(TestHTTPClient, multiple_clients) {
+    std::vector<std::unique_ptr<HTTPClient>> clients;
+
+    uint8_t expected_callbacks_called = 5;
+    uint8_t callbacks_called = 0;
+    std::mutex mutex;
+
+    for (int i = 0 ; i < expected_callbacks_called ; ++i) {
+        std::unique_ptr<HTTPClient> client = std::make_unique<HTTPClient>();
+        ASSERT_TRUE(client->is_ok());
+        EXPECT_TRUE(client->get_async("https://google.com", [&](HTTPReply &reply) {
+            EXPECT_EQ(reply.host, "google.com");
+            EXPECT_EQ(reply.data, expected_google_301);
+            EXPECT_EQ(reply.res_code, 301);
+            std::lock_guard lock(mutex);
+            ++callbacks_called;
+        }, { "Accept: application/json" }));
+        clients.push_back(std::move(client));
+    }
+
+    for (auto &client : clients)
+        client->wait_async_requests();
+
+    EXPECT_EQ(callbacks_called, expected_callbacks_called);
+}
+
+TEST_F(TestHTTPClient, failures) {
+    logger->info("Errors are expected in this test.");
+    HTTPClient client;
+    ASSERT_TRUE(client.is_ok());
+    HTTPReply reply;
+
+    EXPECT_FALSE(client.get(reply, ""));
+
+    EXPECT_FALSE(client.post(reply, "", "test"));
+    EXPECT_FALSE(client.post(reply, "http://google.com", ""));
+
+    EXPECT_FALSE(client.get_async("", [](HTTPReply &reply){ (void)reply; }));
+
+    EXPECT_FALSE(client.post_async("", "test", [](HTTPReply &reply){ (void)reply; }));
+    EXPECT_FALSE(client.post_async("https://google.com", "", [](HTTPReply &reply){ (void)reply; }));
+}
+
+TEST_F(TestHTTPClient, nested) {
+    HTTPClient client;
+    ASSERT_TRUE(client.is_ok());
+
+    uint16_t count = 0;
+    std::mutex mutex;
+
+    for (int i = 0 ; i < 10 ; ++i) {
+        bool res = client.get_async("google.com", [&client, &count, &mutex](HTTPReply &reply) {
+            (void)reply;
+            if (client.get(reply, "google.com")) {
+                {
+                    std::lock_guard lock(mutex);
+                    ++count;
+                }
+            }
+        });
+        EXPECT_TRUE(res);
+    }
+    client.wait_async_requests();
+
+    EXPECT_EQ(count, 10);
+}
+
+TEST_F(TestHTTPClient, early_dtor) {
+    HTTPClient *client = new HTTPClient;
+    ASSERT_TRUE(client->is_ok());
+
+    client->get_async("google.com", [](HTTPReply &reply) {
+        (void)reply;
+    });
+
+    EXPECT_NO_THROW(delete client);
+}
+
+} // namespace test
