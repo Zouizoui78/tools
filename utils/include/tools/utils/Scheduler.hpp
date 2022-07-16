cdf25a38f90fec8cb5fefeb8ccab8577ea930a03
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2022-07-16 14:57:41 +0200

    Added single threaded Scheduler.

---
+++ utils/include/tools/utils/Scheduler.hpp
@@ -0,0 +1,47 @@
+#ifndef SCHEDULER_HPP
+#define SCHEDULER_HPP
+
+#include <atomic>
+#include <chrono>
+#include <functional>
+
+namespace tools::utils {
+
+class Scheduler;
+
+struct Task {
+    std::string name;
+    std::function<bool ()> task;
+    uint64_t delay_us;
+
+    private:
+    std::chrono::steady_clock::time_point next_run;
+
+    friend Scheduler;
+};
+
+class Scheduler {
+    public:
+    
+    Scheduler();
+
+    ~Scheduler();
+
+    bool add_task(Task task);
+
+    void start();
+    void stop();
+
+    inline bool is_running() { return _running; }
+
+    private:
+
+    void loop_wrapper();
+
+    std::atomic<bool> _running = false;
+    std::vector<Task> _tasks;
+};
+
+} // namespace tools::utils
+
+#endif // SCHEDULER_HPP
---
+++ utils/src/Scheduler.cpp
@@ -0,0 +1,51 @@
+#include "tools/utils/Scheduler.hpp"
+#include "tools/utils/Log.hpp"
+
+#include <thread>
+
+namespace tools::utils {
+
+static auto logger = new_logger("Scheduler");
+
+Scheduler::Scheduler() {}
+    
+Scheduler::~Scheduler() {}
+
+bool Scheduler::add_task(Task task) {
+    if (is_running() || task.name.empty() || !task.task)
+        return false;
+
+    _tasks.push_back(task);
+    return true;
+}
+
+void Scheduler::start() {
+    auto now = std::chrono::steady_clock::now();
+    for (auto &e : _tasks)
+        e.next_run = now + std::chrono::microseconds(e.delay_us);
+    _running = true;
+    loop_wrapper();
+}
+
+void Scheduler::stop() {
+    _running = false;
+}
+
+void Scheduler::loop_wrapper() {
+    while (is_running()) {
+        for (auto &e : _tasks) {
+            if (!is_running())
+                break;
+            
+            auto now = std::chrono::steady_clock::now();
+            if (now > e.next_run) {
+                if (!e.task())
+                    logger->error("Task '{}' returned false.", e.name);
+                e.next_run = now + std::chrono::microseconds(e.delay_us);
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+    }
+}
+
+} // namespace tools::utils
