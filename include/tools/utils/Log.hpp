32e71250e85639fd69019fc46d8142ce56974930
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2022-11-19 12:42:31 +0100

    Enable debug log level

--- include/tools/utils/Log.hpp
+++ include/tools/utils/Log.hpp
@@ -1,6 +1,10 @@
 #ifndef LOG_HPP
 #define LOG_HPP
 
+#ifdef DEBUG
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
+#endif
+
 #define SPDLOG_FMT_EXTERNAL
 #define SPDLOG_COMPILED_LIB
 #include <spdlog/spdlog.h>
