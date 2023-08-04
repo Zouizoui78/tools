1ccaa4f9a92b5426078d5a4dda275180f325966b
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-08-04 23:03:03 +0200

    added WaveformGenerator::reset_samples()

--- src/waveform/src/WaveformGenerator.cpp
+++ src/waveform/src/WaveformGenerator.cpp
@@ -5,19 +5,19 @@ namespace tools::waveform {
 
 bool WaveformGenerator::add_waveform(std::shared_ptr<AWaveform> sound) {
     std::lock_guard lock(_mutex);
-    auto it = std::find(_sounds.begin(), _sounds.end(), sound);
-    if (it != _sounds.end())
+    auto it = std::find(_waveforms.begin(), _waveforms.end(), sound);
+    if (it != _waveforms.end())
         return false;
-    _sounds.push_back(sound);
+    _waveforms.push_back(sound);
     return true;
 }
 
 bool WaveformGenerator::remove_waveform(std::shared_ptr<AWaveform> sound) {
     std::lock_guard lock(_mutex);
-    auto it = std::find(_sounds.begin(), _sounds.end(), sound);
-    if (it == _sounds.end())
+    auto it = std::find(_waveforms.begin(), _waveforms.end(), sound);
+    if (it == _waveforms.end())
         return false;
-    _sounds.erase(it);
+    _waveforms.erase(it);
     return true;
 }
 
@@ -26,7 +26,7 @@ double WaveformGenerator::generate_sample() {
     double time = static_cast<double>(_sample_n) / sampling_rate;
     {
         std::lock_guard lock(_mutex);
-        for (auto sound : _sounds) {
+        for (auto sound : _waveforms) {
             ret += sound->synthesize(WaveformTimepoint{ time, _sample_n });
         }
     }
@@ -41,6 +41,10 @@ std::vector<double> WaveformGenerator::generate_n_samples(int n_samples) {
         ret.push_back(generate_sample());
     }
     return ret;
+}
+
+void WaveformGenerator::reset_samples() {
+    _sample_n = 0;
 }
 
 } // namespace tools::waveform
