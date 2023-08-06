fac883ecbb4aab97d000492420e91a9c2e5a9f9e
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-08-06 13:19:16 +0200

    added nyquist frequency constant

--- src/waveform/include/tools/waveform/Constants.hpp
+++ src/waveform/include/tools/waveform/Constants.hpp
@@ -6,6 +6,7 @@
 namespace tools::waveform {
 
 constexpr int32_t sampling_rate = 44100;
+constexpr double nyquist_frequency = sampling_rate / 2.0;
 constexpr double volume_mult = 0.01;
 
 } // namespace tools::waveform
