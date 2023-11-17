51a73ff3e94078d9252f3bcae88f03d9ba8fd307
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-11-17 19:38:12 +0100

    more lenient stopwatch tests

--- test/utils/test_stopwatch.cpp
+++ test/utils/test_stopwatch.cpp
@@ -11,9 +11,12 @@ using namespace std::chrono_literals;
 
 TEST(TestStopwatch, test_stopwatch) {
     Stopwatch s;
-    std::this_thread::sleep_for(1us);
+    auto stop_time = s.get_duration() + 1ms;
+    while (s.get_duration() < stop_time)
+        ;
     auto d = s.get_duration();
-    ASSERT_TRUE(1us < d && d < 2us);
+    std::cout << s.get_duration() << std::endl;
+    ASSERT_TRUE(900us < d && d < 1100us);
 }
 
 TEST(TestStopwatch, test_stopwatch_laps) {
@@ -28,7 +31,7 @@ TEST(TestStopwatch, test_stopwatch_laps) {
 
     auto laps = s.get_laps<microseconds>();
     auto mean = std::accumulate(laps.begin(), laps.end(), 0us) / laps.size();
-    ASSERT_TRUE(995us < mean && mean < 1005us);
+    ASSERT_TRUE(900us < mean && mean < 1100us);
 }
 
 } // namespace test
