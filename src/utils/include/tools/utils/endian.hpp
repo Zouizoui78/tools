079a5b431d65099b82c7f7496bd69acf01bd0346
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2023-11-17 19:31:25 +0100

    add endian::swap_range and constraints on endian inputs

--- src/utils/include/tools/utils/endian.hpp
+++ src/utils/include/tools/utils/endian.hpp
@@ -1,10 +1,14 @@
 #ifndef ENDIAN_HPP
 #define ENDIAN_HPP
 
+#include <ranges>
+#include <type_traits>
+
 namespace tools::endian {
 
 template <typename T>
-T swap_bytes(T in) {
+requires std::is_fundamental_v<T>
+T swap(T in) {
     T swapped = 0;
     char* swapped_as_char = reinterpret_cast<char*>(&swapped);
     char* in_as_char = reinterpret_cast<char*>(&in);
@@ -14,6 +18,17 @@ T swap_bytes(T in) {
         swapped_as_char[i] = in_as_char[type_size - i - 1];
     }
     return swapped;
+}
+
+template <typename R>
+requires std::ranges::input_range<R> &&
+         std::is_fundamental_v<std::ranges::range_value_t<R>>
+R swap_range(R in) {
+    R out{in};
+    for (auto& e : out) {
+        e = swap(e);
+    }
+    return out;
 }
 
 } // namespace tools::endian
