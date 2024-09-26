897a97bd6a74b8b2f526b4c7b26c33975bc101ab
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-09-26 20:34:09 +0200

    add multiple tasks test

--- test/utils/test_thread_pool.cpp
+++ test/utils/test_thread_pool.cpp
@@ -21,4 +21,21 @@ TEST(TestThreadPool, test_thread_pool) {
     ASSERT_TRUE(test);
 }
 
+TEST(TestThreadPool, test_multiple_tasks) {
+    std::atomic<int> counter = 0;
+
+    std::vector<ThreadPool::Task> tasks(
+        {3, {[&counter] {
+             std::cout << "Incrementing counter from thread "
+                       << std::this_thread::get_id() << std::endl;
+             ++counter;
+         }}});
+
+    ThreadPool pool;
+    pool.enqueue_tasks(tasks);
+    pool.wait();
+
+    ASSERT_EQ(counter, tasks.size());
+}
+
 } // namespace test
