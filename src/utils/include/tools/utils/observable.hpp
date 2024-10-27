02dcc2f0895f92beb6a9dc89bc9e26c73f5ea3e4
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-10-27 16:08:31 +0100

    remove Mutexed

--- src/utils/include/tools/utils/observable.hpp
+++ src/utils/include/tools/utils/observable.hpp
@@ -2,9 +2,8 @@
 #define OBSERVABLE_HPP
 
 #include <algorithm>
+#include <mutex>
 #include <vector>
-
-#include "mutexed.hpp"
 
 namespace tools::utils {
 
@@ -26,38 +25,39 @@ public:
 template <typename Event>
 class Observable final {
 private:
-    Mutexed<std::vector<IObserver<Event> *>> _observers;
-    Mutexed<std::vector<IObserver<Event> *>> _observers_to_remove;
+    std::vector<IObserver<Event> *> _observers;
+    std::mutex _observers_mutex;
+
+    std::vector<IObserver<Event> *> _observers_to_remove;
+    std::mutex _observers_to_remove_mutex;
 
 public:
     void notify_observers(const Event &event) {
-        std::scoped_lock observer_lock(_observers);
+        std::scoped_lock observer_lock(_observers_mutex);
 
         {
-            std::scoped_lock to_remove_lock(_observers_to_remove);
+            std::scoped_lock to_remove_lock(_observers_to_remove_mutex);
 
-            std::erase_if(*_observers, [this](IObserver<Event> *current_obs) {
-                return std::ranges::contains(*_observers_to_remove,
-                                             current_obs);
+            std::erase_if(_observers, [this](IObserver<Event> *current_obs) {
+                return std::ranges::contains(_observers_to_remove, current_obs);
             });
 
-            _observers_to_remove->clear();
+            _observers_to_remove.clear();
         }
 
-        std::ranges::for_each(*_observers,
-                              [&event](IObserver<Event> *observer) {
-                                  observer->notify(event);
-                              });
+        std::ranges::for_each(_observers, [&event](IObserver<Event> *observer) {
+            observer->notify(event);
+        });
     }
 
     void add_observer(IObserver<Event> *observer) {
-        std::scoped_lock lock(_observers);
-        _observers->push_back(observer);
+        std::scoped_lock lock(_observers_mutex);
+        _observers.push_back(observer);
     }
 
     void remove_observer(IObserver<Event> *observer) {
-        std::scoped_lock lock_to_remove(_observers_to_remove);
-        _observers_to_remove->emplace_back(observer);
+        std::scoped_lock lock_to_remove(_observers_to_remove_mutex);
+        _observers_to_remove.emplace_back(observer);
     };
 };
 
